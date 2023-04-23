7588a6aee58fef50a198d5f2aab758a39e7fbf86
barroso <franciscobarrosoflf@gmail.com>
2023-04-23 19:29:11 -0300

    rename navBar to Header

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,18 @@
 import { Suspense } from "react";
-import NavBar from "./components/nav-bar";
+import Header from "./components/header";
 import { Route, Routes } from "react-router-dom";
 import About from "./routes/about";
 import Contact from "./routes/contact";
 import NotFound from "./pages/not-found";
+import Board from "./pages/board";
   
 const App = () => {
 	return (
 		<>
-			<NavBar />
+			<Header />
 			<Suspense fallback={<div className="container">Loading...</div>}>
 				<Routes>
-					<Route path="/" element={<About />} />
+					<Route path="/" element={<Board />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/contact" element={<Contact />} />
 					<Route path="*" element={<NotFound />} />
